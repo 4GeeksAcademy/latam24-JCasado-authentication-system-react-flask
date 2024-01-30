122d37fe7db570f06b706ea6db0976db98ff289d
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2024-01-30 20:16:28 +0000

    Getting the front end ready

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+
+    const { store, actions } = useContext( Context )
+    const navigate = useNavigate()
+    const [ data, setData ] = useState({})
+    const handleChange = (event) => {
+
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    return(
+
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <label htmlFor="inputEmail" className="form-label">Email</label>
+                    <input type="text" name="email" id="inputEmail" className="form-control" aria-describedby="emailHelpBlock" onChange={(event) => { handleChange(event) }} />
+                    <div id="emailHelpBlock" className="form-text">
+                        Use a valid email
+                    </div>
+                    <label htmlFor="inputPassword5" className="form-label">Password</label>
+
+                    <input type="password" name="password" id="inputPassword5" className="form-control" aria-describedby="passwordHelpBlock" onChange={ handleChange } />
+                    <div id="passwordHelpBlock" className="form-text">
+                        Use a valid password
+                    </div>
+
+                    <button className="btn btn-primary" onClick={async() => {
+
+                        if ( await actions.makeLogin(data)){
+                            navigate("/")
+                        }
+                     }}>Sign Up
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/front/js/pages/private.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Private = () => {
+
+    return(
+        <h1>Private Route</h1>
+        )
+};
