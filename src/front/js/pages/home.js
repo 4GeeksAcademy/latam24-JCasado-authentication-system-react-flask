cce3eeb4acd10f54447e5be464a20a23ca29f4a4
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2024-02-01 14:25:46 +0000

    home and private fixed

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => {
 
 	const { store, actions } = useContext(Context);
@@ -10,12 +11,12 @@ export const Home = () => {
 	return (
 		
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Hello!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={rigoImageUrl} />	
 			</p>
 			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+				{store.message || "Loading message from the backend (make sure your python backend is running)"}
 			</div>
 			<p>
 				This boilerplate comes with lots of documentation:{" "}
--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,12 +1,36 @@
-import React from "react";
+
+// Confirmar el token para saber si existe, de lo contrario enviar al Login:
+
+import React, { useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+ 
 
 export const Private = () => {
 
-    return(
+    const { store } = useContext(Context)
+    const navigate = useNavigate();
 
-        <h1>Private Route</h1>
-        
-        )
+    useEffect(() => {
+
+        const token = sessionStorage.getItem("token")
+
+        if (!token) {
+
+            alert("Please login or create an account")
+            navigate("/home")
+        }
+    }, [navigate])
+
+    const token = sessionStorage.getItem("token");
+
+    return (
+        <div>
+            {token ? (<h2>Private Route</h2>) : (<h2>Go back to Register or Login</h2>)}
+        </div>
+    )
 };
 
-// Confirmar el token para saber si existe, de lo contrario enviar al Login.
+export default Private
+
+
